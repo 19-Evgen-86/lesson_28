ba3761c9bd86b80f6a86c0565ac0c5daa6a1d3f9
evgeniy <ninjago58rus@gmail.com>
2022-07-08 16:55:46 +0300

    csv_json

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Ads(models.Model):
+    name = models.CharField(max_length=255)
+    author = models.CharField(max_length=100)
+    price = models.IntegerField
+    description = models.TextField
+    address = models.CharField(max_length=255)
+    is_published = models.BooleanField(default=True)
---
+++ datasets/cvs2json.py
@@ -0,0 +1,40 @@
+import csv
+import json
+from pathlib import Path
+
+
+def csv_to_json(input_csv, output_json):
+    result_json = []
+    result_dict = {}
+    with open(input_csv, encoding='utf-8') as file_csv:
+        # читаем  csv file  как список словарей
+        csv_data = csv.DictReader(file_csv)
+        for item in csv_data:
+            if input_csv.split('.')[0] == 'ads':
+                result_dict['model'] = "ads.Ads"
+                result_dict['pk'] = item['id']
+                result_dict['fields'] = {"name": item['name'],
+                                         "author": item['author'],
+                                         "price": item['price'],
+                                         "description": item['description'],
+                                         "address": item['address'],
+                                         "is_published": item['is_published']
+                                         }
+            if input_csv.split('.')[0] == 'categories':
+                result_dict['model'] = "ads.Categories"
+                result_dict['pk'] = item['id']
+                result_dict['fields'] = {"name": item['name']}
+
+            result_json.append(result_dict)
+
+
+    with open(output_json, 'w', encoding='utf-8') as file_json:
+        result = json.dumps(result_json, indent=4, ensure_ascii=False)
+        file_json.write(result)
+
+
+files = Path('.').glob('*.csv')
+for file in files:
+    csv_file = str(file)
+    json_file = str(file).split('.csv')[0] + '.json'
+    csv_to_json(csv_file, json_file)
